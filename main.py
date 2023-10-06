8e676ee194cf08df37c89ae4dea48f35aed49db1
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-10-06 15:32:27 +0400

    find random symbols and numbers

--- main.py
+++ main.py
@@ -10,10 +10,14 @@ nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 password =""
-for letter in range(1,nr_letters+1):
-    random_char = random.choice(letters)
-    password += random_char
-    print(password)
+for char in range(1, nr_letters + 1):
+    password += random.choice(letters)
+
+for char in range(1, nr_symbols + 1):
+    password += random.choice(nr_symbols)
+
+for char in range(1, nr_numbers + 1):
+    password += random.choice(nr_numbers)
 
 
 
